84416392baa9ffde6901679a57f276d000807299
mmogue-2021220 <mmogue-2021220@kinal.edu.gt>
2023-10-07 10:54:15 -0600

    Se agrego estructura del proyecto

---
+++ redbit-api/configs/mongo.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const app = express();
+const port = process.env.PORT || 3020 || 3200;
+
+
+/* ----- CONFIG SERVER ----- */
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+/* ----- IMPORT ROUTES ----- */
+app.use('/user', require('../src/user/user.routes'));
+
+/* ----- DEPLOYED SERVER ----- */
+exports.initServer = () => {
+    app.listen(port);
+    console.log(`Server HTTP running in port ${port}`);
+}
---
+++ redbit-public/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 3033,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:3022',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+                rewrite: path => path.replace(/^\/api/, '')
+            }
+        },
+    }
+})
