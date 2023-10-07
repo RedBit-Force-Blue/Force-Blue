3691a189b0f84501f4c536c2c891fbea2a164b2b
byxmariux <mario.hm205@gmail.com>
2023-10-07 13:49:26 -0600

    feat: :art: structure ready to use

---
+++ redbit-public/src/Index.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import { RouterOutlet } from "./RouterOutlet"
+import { HomePage } from "./pages/HomePage/HomePage"
+import { NotFound } from "./pages/NotFound"
+import { TestPage } from "./pages/TestPage/TestPage"
+
+function App() {
+  try {
+    const routes = createBrowserRouter([
+      {
+        errorElement: <NotFound />,
+        path: "/",
+        element: <RouterOutlet/>,
+        children: [
+          {
+            path: "/",
+            element: <HomePage />,
+          }, 
+          {
+            path: '/testPage', 
+            element:<TestPage/>
+          }
+        ]
+        }
+      ])
+    return (
+     <>
+     <RouterProvider router={routes}/>
+     </>
+    );
+} catch (error) {
+    console.log(error)
+}
+
+}
+export default App
