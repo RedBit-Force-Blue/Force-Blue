fd0b655de52c10deeebd24275c06ee57ae1d2687
byxmariux <mario.hm205@gmail.com>
2023-10-07 16:09:45 -0600

    feat: :sparkles:

--- redbit-public/src/pages/HomePage/HomePage.jsx
+++ redbit-public/src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import SideBarComponent from '../../Components/sideBar/SideBarComponent'
 
 export const HomePage = () => {
   return (
+    <>
+    
     <h1>Hola mundo</h1>
+    <SideBarComponent/>
+    </>
     )
 }
