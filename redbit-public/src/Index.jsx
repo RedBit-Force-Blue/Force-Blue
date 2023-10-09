2f07d4c232fb8938082842b524fb88ef85b14460
gmatta-2021223 <gmatta-2021223@kinal.edu.gt>
2023-10-08 19:13:11 -0600

    Vista de profile.

--- redbit-public/src/Index.jsx
+++ redbit-public/src/Index.jsx
@@ -4,12 +4,13 @@ import { RouterOutlet } from "./RouterOutlet"
 import { HomePage } from "./pages/HomePage/HomePage"
 import { NotFound } from "./pages/NotFound"
 import { TestPage } from "./pages/TestPage/TestPage"
-import {LoginPage} from "./pages/Account/login/LoginPage"
+import { LoginPage } from "./pages/Account/login/LoginPage"
 import { RegisterPage } from "./pages/Account/register/RegisterPage"
 import { ChatPage } from "./pages/Chat/ChatPage"
 import { CardProyect } from "./Components/CardProyect/CardProyect"
 import { CardsList } from "./Components/CardProyect/CardsList"
 import { ChatListPage } from "./pages/Chat/ChatListPage"
+import { Profile } from "./pages/Profile/Profile"
 
 function App() {
   try {
@@ -17,48 +18,52 @@ function App() {
       {
         errorElement: <NotFound />,
         path: "/",
-        element: <RouterOutlet/>,
+        element: <RouterOutlet />,
         children: [
           {
             path: "/",
             element: <HomePage />,
-          }, 
-          {
-            path: '/testPage', 
-            element:<TestPage/>
-          }, 
-          {
-            path :'/Login',
-            element:<LoginPage/>
           },
           {
-            path:'/Register', 
-            element: <RegisterPage/>
+            path: '/testPage',
+            element: <TestPage />
+          },
+          {
+            path: '/Login',
+            element: <LoginPage />
+          },
+          {
+            path: '/Register',
+            element: <RegisterPage />
           },
           {
             path: '/chat',
-            element: <ChatPage/>
+            element: <ChatPage />
           },
           {
             path: '/chatlist',
-            element: <ChatListPage/>
+            element: <ChatListPage />
           },
           {
             path: '/cards',
-            element:<CardsList/>
+            element: <CardsList />
+          },
+          {
+            path: '/profile',
+            element: <Profile />
           }
 
         ]
-        }
-      ])
+      }
+    ])
     return (
-     <>
-     <RouterProvider router={routes}/>
-     </>
+      <>
+        <RouterProvider router={routes} />
+      </>
     );
-} catch (error) {
+  } catch (error) {
     console.log(error)
-}
+  }
 
 }
 export default App
--- redbit-public/src/pages/Profile/Profile.jsx
+++ redbit-public/src/pages/Profile/Profile.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import './Profile.css';
+import profile1 from '../../images/profile1.jpg';
 
-function Profile() {
+export const Profile = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [profileData, setProfileData] = useState({
     usuario: 'Usuario',
@@ -41,9 +43,158 @@ function Profile() {
       [name]: value,
     });
   };
-
   return (
-    <div className="profile">
+
+    <>
+      <div className='bodyProfile'>
+
+        <div class="row py-5 px-2">
+          <div class="col-xl-6 col-md-10 col-sm-10 mx-auto">
+
+
+            <div class="bg-white shadow rounded overflow-hidden">
+              <div class="px-4 bg-dark mb-5">
+
+                <div class=" text-center profile-header">
+
+                  <img src={profile1} alt="..." width="200" height="200" class="rounded mb-2 mx-auto" />
+                  <button href="#" class="btn btn-dark">Edit profile</button>
+
+                </div>
+              </div>
+
+
+              <div class="py-5 px-4">
+
+                <div className="container text-center mt-5">
+
+                  <div className='row mb-3'>
+
+                    <div className='col-6'>
+                      <h5 class="mb-3 fw-bold">Nombres</h5>
+                      <input type="text" class="form-control" placeholder="Nombres" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+
+                    <div className='col-6'>
+                      <h5 class="mb-3 fw-bold">Apellidos</h5>
+                      <input type="text" class="form-control" placeholder="Apellidos" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+
+                  </div>
+
+                  <div className='row mb-3'>
+
+                    <div className='col-6'>
+                      <h5 class="mb-3 fw-bold">Usuario</h5>
+                      <input type="text" class="form-control" placeholder="Usuario" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+
+                    <div className='col-6'>
+                      <h5 class="mb-3 fw-bold">Email</h5>
+                      <input type="text" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+
+                  </div>
+
+                  <div className='row mb-3'>
+                    <div className='col-5'>
+                      <h5 class="mb-3 fw-bold">Teléfono</h5>
+                      <input type="text" class="form-control" placeholder="Teléfono" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+                    <div className='col-4'>
+                      <h5 class="mb-3 fw-bold">Edad</h5>
+                      <input type="number" class="form-control" placeholder="Edad" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+                    <div className='col-3'>
+                      <h5 class="mb-3 fw-bold">Rol</h5>
+                      <input type="text" class="form-control" placeholder="Rol" aria-label="Username" aria-describedby="addon-wrapping" />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="py-4">
+
+                  <h5 class="mb-4 ms-3 h1 fw-bold">My works</h5>
+
+                  <div class="p-4 bg-secondary bg-opacity-25 rounded shadow-sm mb-2">
+
+                    <h1 className='mb-3 h3 fw-bold'>Work Title</h1>
+
+                    <p class="font-italic mb-0">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam.
+                    </p>
+
+                    <ul class="list-inline small text-muted mt-3 mb-0">
+                      <li class="list-inline-item">
+                        <i class="fa fa-comment-o"></i>
+                        2023/07/05
+                      </li>
+                      <li class="list-inline-item">
+                        <i class="bi bi-star-fill text-warning" /><i class="bi bi-star-fill text-warning" /><i class="bi bi-star-fill text-warning" /><i class="bi bi-star-half text-warning" /><i class="bi bi-star text-warning" />
+                      </li>
+                    </ul>
+
+                  </div>
+
+                  <div class="p-4 bg-secondary bg-opacity-25 rounded shadow-sm mb-2">
+
+                    <h1 className='mb-3 h3 fw-bold'>Work Title</h1>
+
+                    <p class="font-italic mb-0">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam.
+                    </p>
+
+                    <ul class="list-inline small text-muted mt-3 mb-0">
+                      <li class="list-inline-item">
+                        <i class="fa fa-comment-o"></i>
+                        2022/04/12
+                      </li>
+                      <li class="list-inline-item">
+                        <i class="bi bi-star-fill text-warning" /><i class="bi bi-star-fill text-warning" /><i class="bi bi-star text-warning" /><i class="bi bi-star text-warning" /><i class="bi bi-star text-warning" />
+                      </li>
+                    </ul>
+
+                  </div>
+
+                  <div class="p-4 bg-secondary bg-opacity-25 rounded shadow-sm">
+
+                    <h1 className='mb-3 h3 fw-bold'>Work Title</h1>
+
+                    <p class="font-italic mb-0">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam.
+                    </p>
+
+                    <ul class="list-inline small text-muted mt-3 mb-0">
+                      <li class="list-inline-item">
+                        <i class="fa fa-comment-o"></i>
+                        2022/01/04
+                      </li>
+                      <li class="list-inline-item">
+                        <i class="bi bi-star-fill text-warning" /><i class="bi bi-star-fill text-warning" /><i class="bi bi-star-fill text-warning" /><i class="bi bi-star-fill text-warning" /><i class="bi bi-star text-warning" />
+                      </li>
+                    </ul>
+
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
+
+  )
+}
+
+{/* <div className="profile">
       <img
         src={profileData.profileImage}
         alt="Foto de perfil"
@@ -51,7 +202,7 @@ function Profile() {
       />
       {isEditing ? (
         <div>
-         
+
           <input
             type="text"
             name="usuario"
@@ -109,8 +260,6 @@ function Profile() {
       <button className="edit-button" onClick={toggleEditing}>
         {isEditing ? 'Guardar' : 'Editar'}
       </button>
-    </div>
-  );
-}
+    </div> */}
 
-export default Profile;
+
